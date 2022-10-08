d39efade059011cb6c5412752f439d96b2494345
Pankhuri-Baranwal <115352009+Pankhuri-Baranwal@users.noreply.github.com>
2022-10-08 18:12:51 +0530

    Create RomanToDecimal.py

---
+++ Python/RomanToDecimal.py
@@ -0,0 +1,25 @@
+#specify the decimal equilant
+tallies = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000,
+}
+#Define a method for conversion logic
+def RomanToDecimal(romanNumeral):
+    sum = 0
+    for i in range(len(romanNumeral) - 1):
+        left = romanNumeral[i]
+        right = romanNumeral[i + 1]
+        if tallies[left] < tallies[right]:
+            sum -= tallies[left]
+        else:
+            sum += tallies[left]
+    sum += tallies[romanNumeral[-1]]
+    return sum
+Roman_Number=input("Enter any Roman number")
+Equilant_Decimal=RomanToDecimal(Roman_Number)
+print(Equilant_Decimal)
